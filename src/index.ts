8afbb3807b1fcec7a086049df0faefc752c54d88
pabZarate <104395946+pabZarate@users.noreply.github.com>
2022-05-01 23:21:13 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+/* Consigna
+En una prueba, un piloto tiene que completar
+4 vueltas
+Se necesita un programa que permita ingresar
+por teclado el tiempo de cada vuelta
+El programa debe retornar el tiempo total y el
+promedio de vuelta
+*/
+
+let vuelta1: number | null = Number(
+  prompt("Tiempo de la primer vuelta (en minutos)", "Por ejemplo: 6.40")
+);
+let vuelta2: number | null = Number(
+  prompt("Tiempo de la segunda vuelta (en minutos)", "Por ejemplo: 6.40")
+);
+let vuelta3: number | null = Number(
+  prompt("Tiempo de la tercer vuelta (en minutos)", "Por ejemplo: 6.40")
+);
+let vuelta4: number | null = Number(
+  prompt("Tiempo de la cuarta vuelta (en minutos)", "Por ejemplo: 6.40")
+);
+
+let tiempoTotal: number = vuelta1 + vuelta2 + vuelta3 + vuelta4;
+let promedioVueltas: number = tiempoTotal / 4;
+
+console.log("El tiempo total fue: " + tiempoTotal, "minutos.");
+console.log("El promedio entre vueltas fue: " + promedioVueltas, "minutos.");
